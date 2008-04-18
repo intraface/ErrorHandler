92706e61f3ef6358ad9c78761146096af94820c5
sune.t.jensen <sune.t.jensen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2008-04-18 08:46:09 +0000

    File observer now removes new lines before loggin errors.

--- 3Party/ErrorHandler/tests/FileLoggerTest.php
+++ 3Party/ErrorHandler/tests/FileLoggerTest.php
@@ -1,21 +1,69 @@
 <?php
-/*
-This seems to be not working!
-class FileErrorLoggerTestCase extends UnitTestCase {
-var $_fh;
-var $_test_file = ‘test.log’;
-function setup() {
-@unlink($this->_test_file);
-$this->_fh = fopen($this->_test_file, ‘w’);
+require_once 'config.test.php';
+
+require_once 'PHPUnit/Framework.php';
+
+require_once 'MDB2.php';
+require_once '../src/ErrorHandler.php';
+require_once '../src/ErrorHandler/Observer/File.php';
+
+class FileLoggerTest_Logger {
+    
+    public $text;
+    
+    function  log($text) {
+        $this->text = $text;
+    }
 }
-function TestRequiresFileHandleToInstantiate() {    }
-function TestWrite() {
-$content = ‘test’.rand(10,100);
-$log =& new FileErrorLogger($this->_fh);
-$log->write($content);
-$file_contents = file_get_contents($this->_test_file);
-$this->assertWantedPattern(‘/’.$content.’$/’, $file_contents);
+
+class FileLoggerTest extends PHPUnit_Framework_TestCase
+{
+    
+    function setUp()
+    {
+        
+    }
+    
+    function createErrorHandler() {
+        
+        $eh = new ErrorHandler();
+        $eh->addObserver(new ErrorHandler_Observer_File(new FileLoggerTest_Logger));
+        return $eh;
+        
+    }
+
+    function testConstructor()
+    {
+        $error = $this->createErrorHandler();
+        $this->assertTrue(is_object($error));
+    }
+
+    function testTriggerError() {
+        
+        $logger = $this->getMock('Logger', array('log'));
+        $logger->expects($this->once())
+                 ->method('log')
+                 ->with($this->equalTo('Error: This is not really an error in myfile line 101 (Request: unknown)'));
+        
+        $error = new ErrorHandler();
+        $error->addObserver(new ErrorHandler_Observer_File($logger));
+        
+        
+        $error->handleError(1, 'This is not really an error', 'myfile', '101', '');
+    }
+    
+    function testTriggerErrorRemovesNewLinesInError() {
+        
+        $logger = $this->getMock('Logger', array('log'));
+        $logger->expects($this->once())
+                 ->method('log')
+                 ->with($this->equalTo('Error: This is not really an error in myfile line 101 (Request: unknown)'));
+        
+        $error = new ErrorHandler();
+        $error->addObserver(new ErrorHandler_Observer_File($logger));
+        
+        
+        $error->handleError(1, "This is not\nreally an error", 'myfile', '101', '');
+    }
 }
-function TestWriteIsTimeStamped() {  }
-}
-*/
+
